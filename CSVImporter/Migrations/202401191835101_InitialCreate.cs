d72c15b732adc3b9a9fac43cf2e678dc36bb4873
Tomáš Kudera <xkudera@mendelu.cz>
2024-01-19 19:39:15 +0100

    Vygenerovaný script pro vytvoření tabulky v DB

---
+++ CSVImporter/Migrations/202401191835101_InitialCreate.cs
@@ -0,0 +1,31 @@
+﻿namespace CSVImporter.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.UserContacts",
+                c => new
+                    {
+                        ContactId = c.Int(nullable: false, identity: true),
+                        m_name = c.String(maxLength: 20),
+                        m_surname = c.String(maxLength: 20),
+                        m_privateIdNumber = c.String(maxLength: 10),
+                        m_address = c.String(maxLength: 50),
+                        m_phoneNumber1 = c.String(maxLength: 9),
+                        m_phoneNumber2 = c.String(maxLength: 9),
+                    })
+                .PrimaryKey(t => t.ContactId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.UserContacts");
+        }
+    }
+}
---
+++ CSVImporter/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace CSVImporter.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CSVImporter.Database.ContactsDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(CSVImporter.Database.ContactsDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
